f86b26d8c6da0baef1737cf051824078894517e5
JonathanPJuhl <cph-jj450@cphbusiness.dk>
2021-12-02 11:26:31 +0100

    almost perfect

--- ImageHandling/ContourCutter.py
+++ ImageHandling/ContourCutter.py
@@ -10,19 +10,17 @@ class ContourCutter:
 
     def contour(self, image, iteration):
         img = cv2.imread('/home/jovyan/Python_eksamen/Images/' + image)
-        ###
         kernel = np.ones((2,2), np.float32)/4
         img = cv2.filter2D(img, -1, kernel)
         plt.imshow(img)
         plt.figure()
-        ###
 
         img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         ret, thresh_H = cv2.threshold(img_grey, 70, 255, 0)
         (_, contours, hierarchy) = cv2.findContours(thresh_H, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
         print("Contours " +str(len(contours)))
        
-        cv2.drawContours(img, contours, -1, 255, 1)
+        cv2.drawContours(img, contours, -1, (0,0,0), 1)
         plt.imshow(img)
         plt.figure()
 
@@ -38,14 +36,18 @@ class ContourCutter:
         plt.figure()
         count = 0
 
-
+        threshold_area = 100 
+        max_area = 1000 
 
         for c in contours:
-            x, y, width, height = cv2.boundingRect(c)
-            img = dst
-            roi = img[y: y + height, x: x + width]
-            cv2.imwrite("/home/jovyan/Python_eksamen/Images/CutImages/"+str(count)+"("+str(iteration)+").jpg", roi)
-            count+=1
+            area = cv2.contourArea(c)         
+            if (area > threshold_area) and (area < max_area) :                   
+            #Put your code in here
+                x, y, width, height = cv2.boundingRect(c)
+                img = dst
+                roi = img[y: y + height, x: x + width]
+                cv2.imwrite("/home/jovyan/Python_eksamen/Images/CutImages/"+str(count)+"("+str(iteration)+").jpg", roi)
+                count+=1
 
 #Figure out how to make contours more accurate
 #make method for reading contours from left to right
