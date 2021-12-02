d78fcb07b3d87d4c984134a397ecb22e3546a871
JonathanPJuhl <cph-jj450@cphbusiness.dk>
2021-12-02 11:13:35 +0100

    pretty darn good rn

--- ImageHandling/ContourCutter.py
+++ ImageHandling/ContourCutter.py
@@ -10,11 +10,23 @@ class ContourCutter:
 
     def contour(self, image, iteration):
         img = cv2.imread('/home/jovyan/Python_eksamen/Images/' + image)
+        ###
+        kernel = np.ones((2,2), np.float32)/4
+        img = cv2.filter2D(img, -1, kernel)
+        plt.imshow(img)
+        plt.figure()
+        ###
+
         img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        ret, thresh_H = cv2.threshold(img_grey, 100, 255, 0)
+        ret, thresh_H = cv2.threshold(img_grey, 70, 255, 0)
         (_, contours, hierarchy) = cv2.findContours(thresh_H, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
         print("Contours " +str(len(contours)))
-        
+       
+        cv2.drawContours(img, contours, -1, 255, 1)
+        plt.imshow(img)
+        plt.figure()
+
+
         mask = np.ones(img.shape[: 2], dtype = "uint8")
         cv2.drawContours(mask, contours, -1, 0, -1)
         mask_inv = cv2.bitwise_not(mask)
@@ -25,7 +37,9 @@ class ContourCutter:
         
         plt.figure()
         count = 0
-      
+
+
+
         for c in contours:
             x, y, width, height = cv2.boundingRect(c)
             img = dst
