3ef0350985af58ecc39a4bbee1baa69b3b840114
JonathanPJuhl <cph-jj450@cphbusiness.dk>
2021-12-12 10:13:41 +0100

    fixed webscraping ipynb

--- FileHandling/NumberGenerator.py
+++ FileHandling/NumberGenerator.py
@@ -1,8 +1,17 @@
+import numpy as np
+
 class NumberGenerator:
     def __init__(self):
         print()
 
     def generateSequence(self, length):
-        numbers = list(range(length))
-        return numbers
+        numbers = [int(x) for x in str(length)]
+        print(numbers)
+        returnVal = ""
+        i = 0
+        while(i<4): 
+            returnVal += str(np.random.choice(numbers))
+            i += 1
+            
+        return returnVal
 
--- Webscraping/DoScrape.py
+++ Webscraping/DoScrape.py
@@ -15,7 +15,7 @@ class DoScrape:
     def __init__(self) -> None:
         pass
 
-    def fakecaptcha_interaction(self, numbers):
+    def fakecaptcha_interaction(self, numbers, iteration):
         ds = DownloadAndSave()
         url = 'https://fakecaptcha.com/'
         options = Options()
@@ -26,9 +26,9 @@ class DoScrape:
         search_field = browser.find_element_by_css_selector("input[name='words'][type='text']")
         search_field.send_keys(numbers)
         search_field.submit()
-        browser.implicitly_wait(10)
+        browser.implicitly_wait(6)
         WebDriverWait(browser, 1000000).until(EC.element_to_be_clickable((By.XPATH, '/html/body/section[2]/div/div[2]/a'))).click()
         img = search_field = browser.find_element_by_css_selector("img[id='words']")
         link = img.get_attribute('src')
-        ds.makeDownload(link, "/home/jovyan/Python_eksamen/Images/01234(400).jpg")
+        ds.makeDownload(link, "/home/jovyan/Python_eksamen/Images/Scraped/" + str(numbers)+"("+str(iteration)+").jpg")
         
