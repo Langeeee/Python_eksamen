a3a0476c81ef566c5269ff601a350064a5f00c8f
JonathanPJuhl <cph-jj450@cphbusiness.dk>
2021-12-12 16:03:56 +0100

    made a load of images

--- Webscraping/DoScrape.py
+++ Webscraping/DoScrape.py
@@ -26,9 +26,10 @@ class DoScrape:
         search_field = browser.find_element_by_css_selector("input[name='words'][type='text']")
         search_field.send_keys(numbers)
         search_field.submit()
-        browser.implicitly_wait(6)
+        browser.implicitly_wait(10)
         WebDriverWait(browser, 1000000).until(EC.element_to_be_clickable((By.XPATH, '/html/body/section[2]/div/div[2]/a'))).click()
         img = search_field = browser.find_element_by_css_selector("img[id='words']")
         link = img.get_attribute('src')
         ds.makeDownload(link, "/home/jovyan/Python_eksamen/Images/Scraped/" + str(numbers)+"("+str(iteration)+").jpg")
-        
+        browser.close()
+        browser.quit()
